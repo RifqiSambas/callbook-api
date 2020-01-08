2258e005c1502ab611923e9c3afdc3d1622ae95f
Rifqi Sambas Khairurrohman <rifqi@sera5.id>
2020-01-08 13:51:27 +0700

    fixx

--- routes/route.js
+++ routes/route.js
@@ -6,9 +6,7 @@ const path = require("path");
 router
 	.get("/book", async (req, res) => {
 		const books = await Book.find();
-		res
-			.status(200)
-			.send(books);
+		res.status(200).send(books);
 	})
 
 	.get("/book/:id", async (req, res) => {
@@ -16,8 +14,7 @@ router
 			const book = await Book.findOne({ _id: req.params.id })
 			res.send(book);
 		} catch {
-			res.status(404)
-			res.send({error : 'buku tidak ditemukan'});
+			res.status(404).send({error : 'buku tidak ditemukan'});
 		}
 	})
 
@@ -30,45 +27,42 @@ router
 				link_author: req.body.link_author
 			});
 			await book.save();
-			res
-				.status(200)
-				.send({message: "berhasil menambah data buku"});
+			res.status(200).send({message: "berhasil menambah data buku"});
 		} catch {
-			res
-				.status(404)
-				.send("gagal menambah data buku")
+			res.status(404).send("gagal menambah data buku")
 		}
 	})
 
-	.put("/book/:id", async (req, res) => {
-		let id = { _id: req.params.id };
-		var data = {
-			title : req.body.title,
-			author : req.body.author,
-			link_author : req.body.link_author,
-			link_book : req.body.link_book
-		};
-	 
-		Book.findByIdAndUpdate(
-			id, 
-			data, 
-			(err, book) => {
-				if (err) throw err;
-				res.send('Sukses, Book berhasil di update - '+book.title);
+	router.put("/book/:id", async (req, res) => {
+		try {
+			const book = await Book.findOne({ _id: req.params.id })
+
+			if (req.body.title) {
+				book.title = req.body.title
 			}
-		);
+			if (req.body.author) {
+				book.content = req.body.author
+			}
+			if (req.body.link_author) {
+				book.content = req.body.author
+			}
+			if (req.body.link_book) {
+				book.content = req.body.author
+			}
+
+			await book.save()
+			res.send(book)
+		} catch {
+			res.status(404).send({ error: "Post doesn't exist!" })
+		}
 	})
 
 	.delete("/book/:id", async (req, res) => {
 		try{
 			await Book.deleteOne({ _id: req.params.id });
-			res
-				.status(204)
-				.send({message: "data buku berhasil dihapus"});
+			res.status(204).send({message: "data buku berhasil dihapus"});
 		} catch{
-			res
-				.status(404)
-				.send({error: "gagal menghapus data buku" });
+			res.status(404).send({error: "gagal menghapus data buku" });
 		}
 	})
 
