af6bf7cb0f3695bf1f4609ee9443779474d4394d
Rifqi Sambas Khairurrohman <rifqi@sera5.id>
2020-01-08 10:05:14 +0700

    add patch method

--- routes/route.js
+++ routes/route.js
@@ -34,10 +34,30 @@ router
 		try{
 			await Book.deleteOne({ _id: req.params.id });
 			res.status(204).send();
-		}
-		catch{
+		} catch{
 			res.status(404);
 			res.send({error: "Buku tidak ditemukan" });
+		}
+	})
+
+	.patch("/book/:id", async (req, res) => {
+		try{
+			const book = await Book.findOne({ _id: req.params.id });
+
+			if(req.body.title)
+				book.title = req.body.title
+			if(req.body.author)
+				book.author = req.body.author
+			if(req.body.link_book)
+				book.link_book = req.body.link_book
+			if(req.body.link_author)
+				book.link_author = req.body.link_author
+
+			await book.save();
+			res.sed(book);
+		} catch{
+			res.status(404);
+			res.send({error: "Buku tidak ditemukan"});
 		}
 	})
 
