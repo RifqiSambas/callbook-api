8e5d37ffeb17ee53ae0ecb9bacac3a864df3472f
Rifqi Sambas Khairurrohman <rifqi@sera5.id>
2020-01-08 08:36:59 +0700

    fixx

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const route = require("./routes/route");
 const port = process.env.PORT || 8080;
-const bodyParser = require('bodyParser');
+const bodyParser = require('body-parser');
 
 mongoose
 	.connect(
