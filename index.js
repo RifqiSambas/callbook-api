3de9ae094f5aa87907256e5a7088a7944bd49e7b
Rifqi Sambas Khairurrohman <rifqi@sera5.id>
2020-01-08 21:29:40 +0700

    enable cors

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ mongoose
 	.then(() => {
 		const app = express();
 		app.use(express.json());
+		app.use(function(req, res, next) {
+			res.header("Access-Control-Allow-Origin", "*");
+			res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+			next();
+		});
 		
 		app.get("/", async(req, res) => {
 			res.sendFile("index.html", {'root': './docs'});
